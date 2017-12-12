fa69540b7d1fc79d225836aad619d342c59c953b
Akhmad Sadullaev <s0556420@htw-berlin.de>
2017-12-12 22:34:38 +0100

    seiten getrennt

---
+++ src/web/details.php
@@ -0,0 +1,18 @@
+<?php
+
+// Gemeinsame genutzte Dinge sind dorthin ausgelagert
+require_once '../bootstrap.php';
+
+//require_once '../tools/reset_database.php';
+
+//Ladet alle Daten aus DB
+$all = $entityManager->getRepository('Tracker\TrackerLine')->findBy([], ['day' => 'ASC']);
+
+$first= $all[0];
+$last = $all[sizeof($all)-1];
+
+
+// Das vorkonfigurierte Twig nutzen, um ein Template (1. Parameter) zu rendern
+// und dabei genau zu bestimmen, welche Variablen verwendet werden sollen
+// (2. Parameter).
+echo $twig->render('details.php.twig', array('lines' => $all, 'first'=>$first, 'last'=>$last));
--- src/web/index.php
+++ src/web/index.php
@@ -14,4 +14,4 @@ $last = $all[sizeof($all)-1];
 // Das vorkonfigurierte Twig nutzen, um ein Template (1. Parameter) zu rendern
 // und dabei genau zu bestimmen, welche Variablen verwendet werden sollen
 // (2. Parameter).
-echo $twig->render('index.php.twig', array('lines' => $all, 'first'=>$first, 'last'=>$last));
+echo $twig->render('gesamt.php.twig', array('lines' => $all, 'first'=>$first, 'last'=>$last));
